aba806aadbc817c1fbc36e69855ac74b4740c09d
KrisPriyargi <72081020+KrisPriyargi@users.noreply.github.com>
2020-10-08 00:14:04 +0700

    Add files via upload

---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+
+    <h1 class="mt-4">
+    	About Me
+	</h1>
+    <img src="<?= BASEURL; ?>/img/kris.JPG" alt="kris" width="170" class="rounded-circle shadow">
+    <p>
+    	Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.
+    </p>
+
+</div>
